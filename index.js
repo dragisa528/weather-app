fe8423a585791b44471c522c92cb5f08ce9ffdd2
techking5287 <techking5287@gmail.com>
2023-06-07 23:01:48 +0100

    Data b/w files issue fixed

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// API key
-const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`
-
 // HTML elements;
 const search = document.querySelector("#search");
 const datalist = document.querySelector("#datalist");
@@ -51,44 +48,7 @@ function addSuggestions() {
     datalist.innerHTML = matchedCities;
 }
 
-
-// object to store data obtain from weather API;
-let weatherData = {};
-
-// function on button;
 function checkButtonEvent() {
-
-    // setting visible to loading;
-    message.innerHTML = "Loading...";
-    message.classList.remove("hide");
-
     const city = search.value;
-
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            weatherData.temperature = `${data["main"]["temp"]}<sup>o</sup>C`;
-            weatherData.atmosphere = data["weather"][0]["description"];
-            weatherData.pressure = data["main"]["pressure"];
-            weatherData.city = data["name"];
-            weatherData.country = data["sys"]["country"];
-
-            let currentTime = getCurrentTime(data["timezone"]);
-            weatherData.time = currentTime.toLocaleTimeString();
-            weatherData.hours = currentTime.getHours();
-
-            message.innerHTML = weatherData.time;
-            window.location.href = "weather.html";
-    });
-}
-
-// caculate time from timezone;
-function getCurrentTime(timezone) {
-    const currentTime = new Date();
-    const localTimezoneOffset = currentTime.getTimezoneOffset() * 60; // Get local timezone offset in seconds
-    const targetTime = new Date(currentTime.getTime() + (timezone + localTimezoneOffset) * 1000);
-  
-    return targetTime;
+    window.location.href = `weather.html?city=${city}`;
 }
